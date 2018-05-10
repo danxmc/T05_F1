5c1f4eed296a03991038f10d0c3ff4ecd07c4840
danxmc <danx_97@hotmail.com>
2018-05-10 14:00:01 -0500

    Rev SED

--- Assets/Scripts/SpawnObjects.cs
+++ Assets/Scripts/SpawnObjects.cs
@@ -22,7 +22,6 @@ public class SpawnObjects : MonoBehaviour {
     {
         Vector3 pos = center + new Vector3(Random.Range(-size.x/2, size.x / 2), Random.Range(-size.y / 2, size.y / 2), Random.Range(-size.z /2 , size.z/2));
         Instantiate(blockPrefab, pos, Quaternion.identity);
-
     }
 
 	void OnDrawGizmosSelected()
